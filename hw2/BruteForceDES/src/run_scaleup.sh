42cc700998b21f17dd79f9ed1ed1cc2c3c3096e2
luzerno <yifan.ge@outlook.com>
2013-03-07 05:33:33 +0000

    give a hug

--- hw2/BruteForceDES/src/run_scaleup.sh
+++ hw2/BruteForceDES/src/run_scaleup.sh
@@ -1,128 +1,40 @@
 echo "************* thread = 1" > scaleup
 echo "************* thread = 1"
-java BruteForceDES 1 5 >> scaleup
-java BruteForceDES 1 5 >> scaleup
-java BruteForceDES 1 5 >> scaleup
-java BruteForceDES 1 5 >> scaleup
-java BruteForceDES 1 5 >> scaleup
+java BruteForceDES 1 20 >> scaleup
+java BruteForceDES 1 20 >> scaleup
+java BruteForceDES 1 20 >> scaleup
+java BruteForceDES 1 20 >> scaleup
+java BruteForceDES 1 20 >> scaleup
 
 echo "************* thread = 2" >> scaleup
 echo "************* thread = 2"
-java BruteForceDES 2 10 >> scaleup
-java BruteForceDES 2 10 >> scaleup
-java BruteForceDES 2 10 >> scaleup
-java BruteForceDES 2 10 >> scaleup
-java BruteForceDES 2 10 >> scaleup
-
-echo "************* thread = 3" >> scaleup
-echo "************* thread = 3"
-java BruteForceDES 3 15 >> scaleup
-java BruteForceDES 3 15 >> scaleup
-java BruteForceDES 3 15 >> scaleup
-java BruteForceDES 3 15 >> scaleup
-java BruteForceDES 3 15 >> scaleup
+java BruteForceDES 2 21 >> scaleup
+java BruteForceDES 2 21 >> scaleup
+java BruteForceDES 2 21 >> scaleup
+java BruteForceDES 2 21 >> scaleup
+java BruteForceDES 2 21 >> scaleup
 
 echo "************* thread = 4" >> scaleup
 echo "************* thread = 4"
-java BruteForceDES 4 20 >> scaleup
-java BruteForceDES 4 20 >> scaleup
-java BruteForceDES 4 20 >> scaleup
-java BruteForceDES 4 20 >> scaleup
-java BruteForceDES 4 20 >> scaleup
+java BruteForceDES 4 22 >> scaleup
+java BruteForceDES 4 22 >> scaleup
+java BruteForceDES 4 22 >> scaleup
+java BruteForceDES 4 22 >> scaleup
+java BruteForceDES 4 22 >> scaleup
 
-echo "************* thread = 5" >> scaleup
-echo "************* thread = 5"
-java BruteForceDES 5 25 >> scaleup
-java BruteForceDES 5 25 >> scaleup
-java BruteForceDES 5 25 >> scaleup
-java BruteForceDES 5 25 >> scaleup
-java BruteForceDES 5 25 >> scaleup
+echo "************* thread = 8" >> scaleup
+echo "************* thread = 8"
+java BruteForceDES 8 23 >> scaleup
+java BruteForceDES 8 23 >> scaleup
+java BruteForceDES 8 23 >> scaleup
+java BruteForceDES 8 23 >> scaleup
+java BruteForceDES 8 23 >> scaleup
 
-echo "************* thread = 6" >> scaleup
-echo "************* thread = 6"
-java BruteForceDES 6 30 >> scaleup
-java BruteForceDES 6 30 >> scaleup
-java BruteForceDES 6 30 >> scaleup
-java BruteForceDES 6 30 >> scaleup
-java BruteForceDES 6 30 >> scaleup
-
-#echo "************* thread = 7" >> scaleup
-#echo "************* thread = 7"
-#java BruteForceDES 7 7000000000 >> scaleup
-#java BruteForceDES 7 7000000000 >> scaleup
-#java BruteForceDES 7 7000000000 >> scaleup
-#java BruteForceDES 7 7000000000 >> scaleup
-#java BruteForceDES 7 7000000000 >> scaleup
-#
-#echo "************* thread = 8" >> scaleup
-#echo "************* thread = 8"
-#java BruteForceDES 8 8000000000 >> scaleup
-#java BruteForceDES 8 8000000000 >> scaleup
-#java BruteForceDES 8 8000000000 >> scaleup
-#java BruteForceDES 8 8000000000 >> scaleup
-#java BruteForceDES 8 8000000000 >> scaleup
-#
-#echo "************* thread = 9" >> scaleup
-#echo "************* thread = 9"
-#java BruteForceDES 9 9000000000 >> scaleup
-#java BruteForceDES 9 9000000000 >> scaleup
-#java BruteForceDES 9 9000000000 >> scaleup
-#java BruteForceDES 9 9000000000 >> scaleup
-#java BruteForceDES 9 9000000000 >> scaleup
-#
-#echo "************* thread = 10" >> scaleup
-#echo "************* thread = 10"
-#java BruteForceDES 10 10000000000 >> scaleup
-#java BruteForceDES 10 10000000000 >> scaleup
-#java BruteForceDES 10 10000000000 >> scaleup
-#java BruteForceDES 10 10000000000 >> scaleup
-#java BruteForceDES 10 10000000000 >> scaleup
-#
-#echo "************* thread = 11" >> scaleup
-#echo "************* thread = 11"
-#java BruteForceDES 11 11000000000 >> scaleup
-#java BruteForceDES 11 11000000000 >> scaleup
-#java BruteForceDES 11 11000000000 >> scaleup
-#java BruteForceDES 11 11000000000 >> scaleup
-#java BruteForceDES 11 11000000000 >> scaleup
-#
-#echo "************* thread = 12" >> scaleup
-#echo "************* thread = 12"
-#java BruteForceDES 12 12000000000 >> scaleup
-#java BruteForceDES 12 12000000000 >> scaleup
-#java BruteForceDES 12 12000000000 >> scaleup
-#java BruteForceDES 12 12000000000 >> scaleup
-#java BruteForceDES 12 12000000000 >> scaleup
-#
-#echo "************* thread = 13" >> scaleup
-#echo "************* thread = 13"
-#java BruteForceDES 13 13000000000 >> scaleup
-#java BruteForceDES 13 13000000000 >> scaleup
-#java BruteForceDES 13 13000000000 >> scaleup
-#java BruteForceDES 13 13000000000 >> scaleup
-#java BruteForceDES 13 13000000000 >> scaleup
-#
-#echo "************* thread = 14" >> scaleup
-#echo "************* thread = 14"
-#java BruteForceDES 14 14000000000 >> scaleup
-#java BruteForceDES 14 14000000000 >> scaleup
-#java BruteForceDES 14 14000000000 >> scaleup
-#java BruteForceDES 14 14000000000 >> scaleup
-#java BruteForceDES 14 14000000000 >> scaleup
-#
-#echo "************* thread = 15" >> scaleup
-#echo "************* thread = 15"
-#java BruteForceDES 15 15000000000 >> scaleup
-#java BruteForceDES 15 15000000000 >> scaleup
-#java BruteForceDES 15 15000000000 >> scaleup
-#java BruteForceDES 15 15000000000 >> scaleup
-#java BruteForceDES 15 15000000000 >> scaleup
-#
-#echo "************* thread = 16" >> scaleup
-#echo "************* thread = 16"
-#java BruteForceDES 16 16000000000 >> scaleup
-#java BruteForceDES 16 16000000000 >> scaleup
-#java BruteForceDES 16 16000000000 >> scaleup
-#java BruteForceDES 16 16000000000 >> scaleup
-#java BruteForceDES 16 16000000000 >> scaleup
+echo "************* thread = 16" >> scaleup
+echo "************* thread = 16"
+java BruteForceDES 16 24 >> scaleup
+java BruteForceDES 16 24 >> scaleup
+java BruteForceDES 16 24 >> scaleup
+java BruteForceDES 16 24 >> scaleup
+java BruteForceDES 16 24 >> scaleup
 
--- hw2/BruteForceDES/src/run_speedup.sh
+++ hw2/BruteForceDES/src/run_speedup.sh
@@ -1,128 +1,128 @@
 echo "************* thread = 1" > speedup
 echo "************* thread = 1" 
-java CoinFlip 1 20 >> speedup
-java CoinFlip 1 20 >> speedup
-java CoinFlip 1 20 >> speedup
-java CoinFlip 1 20 >> speedup
-java CoinFlip 1 20 >> speedup
+java BruteForceDES 1 20 >> speedup
+java BruteForceDES 1 20 >> speedup
+java BruteForceDES 1 20 >> speedup
+java BruteForceDES 1 20 >> speedup
+java BruteForceDES 1 20 >> speedup
 
 echo "************* thread = 2" >> speedup
 echo "************* thread = 2"
-java CoinFlip 2 20 >> speedup
-java CoinFlip 2 20 >> speedup
-java CoinFlip 2 20 >> speedup
-java CoinFlip 2 20 >> speedup
-java CoinFlip 2 20 >> speedup
+java BruteForceDES 2 20 >> speedup
+java BruteForceDES 2 20 >> speedup
+java BruteForceDES 2 20 >> speedup
+java BruteForceDES 2 20 >> speedup
+java BruteForceDES 2 20 >> speedup
 
 echo "************* thread = 3" >> speedup
 echo "************* thread = 3"
-java CoinFlip 3 20 >> speedup
-java CoinFlip 3 20 >> speedup
-java CoinFlip 3 20 >> speedup
-java CoinFlip 3 20 >> speedup
-java CoinFlip 3 20 >> speedup
+java BruteForceDES 3 20 >> speedup
+java BruteForceDES 3 20 >> speedup
+java BruteForceDES 3 20 >> speedup
+java BruteForceDES 3 20 >> speedup
+java BruteForceDES 3 20 >> speedup
 
 echo "************* thread = 4" >> speedup
 echo "************* thread = 4"
-java CoinFlip 4 20 >> speedup
-java CoinFlip 4 20 >> speedup
-java CoinFlip 4 20 >> speedup
-java CoinFlip 4 20 >> speedup
-java CoinFlip 4 20 >> speedup
+java BruteForceDES 4 20 >> speedup
+java BruteForceDES 4 20 >> speedup
+java BruteForceDES 4 20 >> speedup
+java BruteForceDES 4 20 >> speedup
+java BruteForceDES 4 20 >> speedup
 
 echo "************* thread = 5" >> speedup
 echo "************* thread = 5"
-java CoinFlip 5 20 >> speedup
-java CoinFlip 5 20 >> speedup
-java CoinFlip 5 20 >> speedup
-java CoinFlip 5 20 >> speedup
-java CoinFlip 5 20 >> speedup
+java BruteForceDES 5 20 >> speedup
+java BruteForceDES 5 20 >> speedup
+java BruteForceDES 5 20 >> speedup
+java BruteForceDES 5 20 >> speedup
+java BruteForceDES 5 20 >> speedup
 
 echo "************* thread = 6" >> speedup
 echo "************* thread = 6"
-java CoinFlip 6 20 >> speedup
-java CoinFlip 6 20 >> speedup
-java CoinFlip 6 20 >> speedup
-java CoinFlip 6 20 >> speedup
-java CoinFlip 6 20 >> speedup
+java BruteForceDES 6 20 >> speedup
+java BruteForceDES 6 20 >> speedup
+java BruteForceDES 6 20 >> speedup
+java BruteForceDES 6 20 >> speedup
+java BruteForceDES 6 20 >> speedup
 
 echo "************* thread = 7" >> speedup
 echo "************* thread = 7"
-java CoinFlip 7 20 >> speedup
-java CoinFlip 7 20 >> speedup
-java CoinFlip 7 20 >> speedup
-java CoinFlip 7 20 >> speedup
-java CoinFlip 7 20 >> speedup
+java BruteForceDES 7 20 >> speedup
+java BruteForceDES 7 20 >> speedup
+java BruteForceDES 7 20 >> speedup
+java BruteForceDES 7 20 >> speedup
+java BruteForceDES 7 20 >> speedup
 
 echo "************* thread = 8" >> speedup
 echo "************* thread = 8"
-java CoinFlip 8 20 >> speedup
-java CoinFlip 8 20 >> speedup
-java CoinFlip 8 20 >> speedup
-java CoinFlip 8 20 >> speedup
-java CoinFlip 8 20 >> speedup
+java BruteForceDES 8 20 >> speedup
+java BruteForceDES 8 20 >> speedup
+java BruteForceDES 8 20 >> speedup
+java BruteForceDES 8 20 >> speedup
+java BruteForceDES 8 20 >> speedup
 
 echo "************* thread = 9" >> speedup
 echo "************* thread = 9"
-java CoinFlip 9 20 >> speedup
-java CoinFlip 9 20 >> speedup
-java CoinFlip 9 20 >> speedup
-java CoinFlip 9 20 >> speedup
-java CoinFlip 9 20 >> speedup
+java BruteForceDES 9 20 >> speedup
+java BruteForceDES 9 20 >> speedup
+java BruteForceDES 9 20 >> speedup
+java BruteForceDES 9 20 >> speedup
+java BruteForceDES 9 20 >> speedup
 
 echo "************* thread = 10" >> speedup
 echo "************* thread = 10"
-java CoinFlip 10 20 >> speedup
-java CoinFlip 10 20 >> speedup
-java CoinFlip 10 20 >> speedup
-java CoinFlip 10 20 >> speedup
-java CoinFlip 10 20 >> speedup
+java BruteForceDES 10 20 >> speedup
+java BruteForceDES 10 20 >> speedup
+java BruteForceDES 10 20 >> speedup
+java BruteForceDES 10 20 >> speedup
+java BruteForceDES 10 20 >> speedup
 
 echo "************* thread = 11" >> speedup
 echo "************* thread = 11"
-java CoinFlip 11 20 >> speedup
-java CoinFlip 11 20 >> speedup
-java CoinFlip 11 20 >> speedup
-java CoinFlip 11 20 >> speedup
-java CoinFlip 11 20 >> speedup
+java BruteForceDES 11 20 >> speedup
+java BruteForceDES 11 20 >> speedup
+java BruteForceDES 11 20 >> speedup
+java BruteForceDES 11 20 >> speedup
+java BruteForceDES 11 20 >> speedup
 
 echo "************* thread = 12" >> speedup
 echo "************* thread = 12"
-java CoinFlip 12 20 >> speedup
-java CoinFlip 12 20 >> speedup
-java CoinFlip 12 20 >> speedup
-java CoinFlip 12 20 >> speedup
-java CoinFlip 12 20 >> speedup
+java BruteForceDES 12 20 >> speedup
+java BruteForceDES 12 20 >> speedup
+java BruteForceDES 12 20 >> speedup
+java BruteForceDES 12 20 >> speedup
+java BruteForceDES 12 20 >> speedup
 
 echo "************* thread = 13" >> speedup
 echo "************* thread = 13"
-java CoinFlip 13 20 >> speedup
-java CoinFlip 13 20 >> speedup
-java CoinFlip 13 20 >> speedup
-java CoinFlip 13 20 >> speedup
-java CoinFlip 13 20 >> speedup
+java BruteForceDES 13 20 >> speedup
+java BruteForceDES 13 20 >> speedup
+java BruteForceDES 13 20 >> speedup
+java BruteForceDES 13 20 >> speedup
+java BruteForceDES 13 20 >> speedup
 
 echo "************* thread = 14" >> speedup
 echo "************* thread = 14"
-java CoinFlip 14 20 >> speedup
-java CoinFlip 14 20 >> speedup
-java CoinFlip 14 20 >> speedup
-java CoinFlip 14 20 >> speedup
-java CoinFlip 14 20 >> speedup
+java BruteForceDES 14 20 >> speedup
+java BruteForceDES 14 20 >> speedup
+java BruteForceDES 14 20 >> speedup
+java BruteForceDES 14 20 >> speedup
+java BruteForceDES 14 20 >> speedup
 
 echo "************* thread = 15" >> speedup
 echo "************* thread = 15"
-java CoinFlip 15 20 >> speedup
-java CoinFlip 15 20 >> speedup
-java CoinFlip 15 20 >> speedup
-java CoinFlip 15 20 >> speedup
-java CoinFlip 15 20 >> speedup
+java BruteForceDES 15 20 >> speedup
+java BruteForceDES 15 20 >> speedup
+java BruteForceDES 15 20 >> speedup
+java BruteForceDES 15 20 >> speedup
+java BruteForceDES 15 20 >> speedup
 
 echo "************* thread = 16" >> speedup
 echo "************* thread = 16"
-java CoinFlip 16 20 >> speedup
-java CoinFlip 16 20 >> speedup
-java CoinFlip 16 20 >> speedup
-java CoinFlip 16 20 >> speedup
-java CoinFlip 16 20 >> speedup
+java BruteForceDES 16 20 >> speedup
+java BruteForceDES 16 20 >> speedup
+java BruteForceDES 16 20 >> speedup
+java BruteForceDES 16 20 >> speedup
+java BruteForceDES 16 20 >> speedup
 
