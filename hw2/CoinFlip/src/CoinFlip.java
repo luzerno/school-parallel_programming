6c06be39f6c2cd779274a78ed32660005f3699e8
Yifan Ge <yifan.ge@outlook.com>
2013-03-07 20:55:24 -0500

    please

--- hw2/CoinFlip/src/CoinFlip.java
+++ hw2/CoinFlip/src/CoinFlip.java
@@ -4,14 +4,11 @@ public class CoinFlip {
     private static CoinThread[] coinThreads;
     private static Thread[] threads;
     private static long startTime, endTime;
-    private static long startupStartTime, startupEndTime;    
     
     private static void flip() {
-    	startTime = System.currentTimeMillis();
     	for (int i = 0; i < numThreads; i++) {
     		threads[i].start();
     	}
-        startupEndTime = System.currentTimeMillis();	
     	for (int i = 0; i < numThreads; i++) {
     		try {
     			threads[i].join();
@@ -29,7 +26,6 @@ public class CoinFlip {
     	}
     	System.out.println(numHeads + " heads in " + numIterations + " coin tosses.");
     	System.out.println("Elapsed time: " + (endTime - startTime) + "ms");
-        System.out.println("Startup time: " + (startupEndTime - startTime) + "ms");
     	
     }
     private static void initCoinThreads() {
@@ -65,7 +61,7 @@ public class CoinFlip {
     
     public static void main(String[] args) {
         parseArgs(args);
-        startupStartTime = System.currentTimeMillis();
+        startTime = System.currentTimeMillis();
         initCoinThreads();
         flip();
         collectResults();
